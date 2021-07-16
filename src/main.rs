6405fffa67d51ee89d897fc32b37bca52c82b9da
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2021-07-16 14:14:51 -0500

    crud functions are working

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,30 @@ struct Args {
 
 #[derive(StructOpt)]
 enum HizteryCmd {
-    Add { history_item: String },
-    Update { history_id: u64 },
-    Delete { history_id: u64 },
+    Insert {
+        history_item: String,
+        rows_to_insert: i64,
+    },
+    Update {
+        history_id: i64,
+        history_item: String,
+    },
+    Delete {
+        history_id: i64,
+    },
     Select {},
+}
+
+#[derive(Debug, sqlx::FromRow)]
+struct HistoryTable {
+    history_id: i64,
+    session_id: i64,
+    history_item: String,
+    // This is why datetime is a string
+    // https://www.sqlite.org/datatype3.html
+    // https://www.sqlite.org/lang_datefunc.html
+    datetime: String,
+    executions: i64,
 }
 
 #[async_std::main]
@@ -25,20 +45,23 @@ async fn main(args: Args) -> anyhow::Result<()> {
     initialize_db(&pool).await?;
 
     match args.cmd {
-        Some(HizteryCmd::Add { history_item }) => {
-            for x in 1000..2000 {
+        // cargo run -- insert "some history message" 5
+        Some(HizteryCmd::Insert {
+            history_item,
+            rows_to_insert,
+        }) => {
+            for row in 0..rows_to_insert {
                 let hist_item_clone = history_item.clone();
-                // print!("Adding new history item description '{}'", &hist_item_clone);
                 let start_time = Local::now();
                 // println!("formatting time");
                 let formatted_start_time = start_time.format("%Y-%m-%d %H:%M:%S").to_string();
                 // println!("running add_bogus_entry");
                 let history_id =
-                    add_bogus_entry(&pool, x, &hist_item_clone, formatted_start_time).await?;
+                    insert_history_item(&pool, row + 100, &hist_item_clone, formatted_start_time)
+                        .await?;
                 // println!("calculating end time");
                 let end_time = Local::now();
                 // println!("calculating duration");
-                // let insert_ms = (end_time - start_time);
                 let insert_time_ms = if let Some(time) = (end_time - start_time).num_microseconds()
                 {
                     time as f64 / 1000.0 as f64
@@ -46,32 +69,96 @@ async fn main(args: Args) -> anyhow::Result<()> {
                     0.0 as f64
                 };
                 print!(
-                    "Added new history entry: [{}] with id: {:?} in: {} ms.",
+                    "Insert new history item: [{}] with id: [{:?}] in: [{}] ms.",
                     &hist_item_clone, history_id, insert_time_ms
                 );
-                let perf_update_id = update_perf(&pool, history_id, insert_time_ms).await?;
+                let perf_update_id = insert_perf_with(&pool, history_id, insert_time_ms).await?;
                 println!(
-                    " Successfully updated the perf table with id: [{}]",
-                    perf_update_id
+                    " Successfully updated perf table with id: [{}] perf: [{}].",
+                    perf_update_id, insert_time_ms
                 );
             }
         }
-        Some(HizteryCmd::Update { history_id }) => {
-            println!("Marking todo {} as done", history_id);
-            // if complete_todo(&pool, id).await? {
-            //     println!("Todo {} is marked as done", id);
-            // } else {
-            //     println!("Invalid id {}", id);
-            // }
+        Some(HizteryCmd::Update {
+            history_id,
+            history_item,
+        }) => {
+            // cargo run -- update 1 "some string"
+            println!(
+                "Updating history_id: [{}] with history_item: [{}]",
+                history_id, history_item
+            );
+            let row = update_history_item(&pool, history_id, history_item).await?;
+            println!("Updated row: [{}]", row);
         }
-        Some(HizteryCmd::Delete { history_id }) => {}
+        Some(HizteryCmd::Delete { history_id }) => {
+            // cargo run -- delete 3
+            println!("Deleting history item: [{}]", history_id);
+            let res = delete_history_item(&pool, history_id).await?;
+            println!("Deleted row count: [{}]", res);
+        }
         Some(HizteryCmd::Select {}) | None => {
-            println!("Printing list of all todos");
-            // list_todos(&pool).await?;
+            // cargo run -- select
+            println!("List of top 5 history items");
+            let output = select_star(&pool).await?;
+            let mut count = 0;
+            for x in output {
+                if count > 5 {
+                    break;
+                } else {
+                    println!("ItemNum: [{}] Row: [{:?}]", count, x);
+                    count += 1;
+                }
+            }
         }
     }
 
     Ok(())
+}
+
+async fn delete_history_item(pool: &SqlitePool, id: i64) -> anyhow::Result<u64> {
+    let mut conn = pool.acquire().await?;
+
+    let query = format!("DELETE FROM history WHERE history_id = '{}';", id);
+
+    let res = sqlx::query(&query)
+        .execute(&mut conn)
+        .await?
+        .rows_affected();
+
+    Ok(res)
+}
+
+async fn update_history_item(pool: &SqlitePool, id: i64, text: String) -> anyhow::Result<u64> {
+    let mut conn = pool.acquire().await?;
+
+    let query = format!(
+        "UPDATE history SET (history_item) = '{}' WHERE history_id = {}",
+        text, id
+    );
+
+    let res = sqlx::query(&query)
+        .execute(&mut conn)
+        .await?
+        .rows_affected();
+
+    Ok(res)
+}
+
+async fn select_star(pool: &SqlitePool) -> anyhow::Result<Vec<HistoryTable>> {
+    let mut conn = pool.acquire().await?;
+
+    let query = r#"
+    SELECT history_id,  session_id,  history_item,  datetime,  executions
+    FROM history;
+    "#;
+
+    // let query_output: Vec<HistoryTable> = sqlx::query_as!(HistoryTable, "select * from history")
+    //     .fetch_all(&mut conn)
+    //     .await?;
+
+    let query_output: Vec<HistoryTable> = sqlx::query_as(&query).fetch_all(&mut conn).await?;
+    Ok(query_output)
 }
 
 async fn initialize_db(pool: &SqlitePool) -> anyhow::Result<bool> {
@@ -81,7 +168,7 @@ async fn initialize_db(pool: &SqlitePool) -> anyhow::Result<bool> {
         "history_id"      INTEGER PRIMARY KEY NOT NULL,
         "session_id"      INTEGER             NOT NULL,
         "history_item"    TEXT                NOT NULL,
-        "datetime"        DATETIME            NOT NULL,
+        "datetime"        TEXT                NOT NULL,
         "executions"      INTEGER             NOT NULL
     );
     "#;
@@ -102,7 +189,11 @@ async fn initialize_db(pool: &SqlitePool) -> anyhow::Result<bool> {
     Ok(true)
 }
 
-async fn update_perf(pool: &SqlitePool, history_id: i64, insert_ms: f64) -> anyhow::Result<i64> {
+async fn insert_perf_with(
+    pool: &SqlitePool,
+    history_id: i64,
+    insert_ms: f64,
+) -> anyhow::Result<i64> {
     let mut conn = pool.acquire().await?;
 
     // println!("creating query");
@@ -110,7 +201,7 @@ async fn update_perf(pool: &SqlitePool, history_id: i64, insert_ms: f64) -> anyh
         "INSERT INTO performance (metrics, history_id) VALUES ({}, {})",
         insert_ms, history_id
     );
-    // println!("executing query {}", &query);
+    // println!("executing query '{}'", &query);
     let id = sqlx::query(&query)
         .execute(&mut conn)
         .await?
@@ -119,7 +210,7 @@ async fn update_perf(pool: &SqlitePool, history_id: i64, insert_ms: f64) -> anyh
     Ok(id)
 }
 
-async fn add_bogus_entry(
+async fn insert_history_item(
     pool: &SqlitePool,
     session_id: i64,
     history_item: &String,
@@ -139,18 +230,6 @@ async fn add_bogus_entry(
         .last_insert_rowid();
 
     // println!("returning id {}", id);
-
-    // Insert the task, then obtain the ID of this row
-    // let id = sqlx::query!(
-    //     r#"
-    //     INSERT INTO todos ( description )
-    //     VALUES ( ?1 )
-    //     "#,
-    //     description
-    // )
-    // .execute(&mut conn)
-    // .await?
-    // .last_insert_rowid();
 
     Ok(id)
 }
